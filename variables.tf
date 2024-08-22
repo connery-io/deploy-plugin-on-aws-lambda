9a26bf77fdd575e7df769fb4a8e5246f0fbfae3a
Volodymyr Machula <machulav@gmail.com>
2024-08-22 16:11:27 +0200

    Add VPC config and custom ENV variables

--- variables.tf
+++ variables.tf
@@ -8,6 +8,8 @@ variable "plugin_version" {
   type        = string
 }
 
+# Lambda function
+
 variable "memory_size" {
   description = "Memory size"
   type        = number
@@ -20,11 +22,7 @@ variable "timeout" {
   default     = 30
 }
 
-variable "resource_name_prefix" {
-  description = "Resource name prefix"
-  type        = string
-  default     = "connery-plugin"
-}
+# Custom policy
 
 variable "attach_policy_statements" {
   description = "Controls whether policy_statements should be added to IAM role for Lambda Function"
@@ -36,4 +34,34 @@ variable "policy_statements" {
   description = "Map of dynamic policy statements to attach to Lambda Function role"
   type        = any
   default     = {}
+}
+
+# VPC deployment
+
+variable "vpc_security_group_ids" {
+  description = "List of security group ids when Lambda Function should run in the VPC."
+  type        = list(string)
+  default     = null
+}
+
+variable "vpc_subnet_ids" {
+  description = "List of subnet ids when Lambda Function should run in the VPC. Usually private or intra subnets."
+  type        = list(string)
+  default     = null
+}
+
+# Custom environment variables
+
+variable "environment_variables" {
+  description = "A map that defines environment variables for the Lambda Function."
+  type        = map(string)
+  default     = {}
+}
+
+# Other
+
+variable "resource_name_prefix" {
+  description = "Resource name prefix"
+  type        = string
+  default     = "connery-plugin"
 }
